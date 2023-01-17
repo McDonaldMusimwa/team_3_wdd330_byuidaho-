d1e91bbe567f722a3e57e05d0b742cb34721d3f6
McDonald <mus22003@byui.edu>
2023-01-17 15:27:30 +0200

    worked on the cart its now working

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,7 +2,10 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
+  const cartItemsList = [];
+  cartItemsList.push(cartItems);
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
