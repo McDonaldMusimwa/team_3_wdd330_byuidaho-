b044b09476eb0d893d57f91d03eed125c42e72cc
McDonald <mus22003@byui.edu>
2023-02-23 20:01:13 +0200

    week 8 by Kigame

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -18,49 +18,77 @@ export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(listcart));
 }
 
-export function renderListWithTemplate(templateFn, parentElement, list, position = 'afterbegin', clear = false){
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
   const htmlStrings = list.map(templateFn);
-  if (clear){
-    parentElement.innerHTML = '';
+  if (clear) {
+    parentElement.innerHTML = "";
   }
-  parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
-  qs(selector).addEventListener('touchend', (event) => {
+  qs(selector).addEventListener("touchend", (event) => {
     event.preventDefault();
     callback();
   });
-  qs(selector).addEventListener('click', callback);
+  qs(selector).addEventListener("click", callback);
 }
 
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get('product')
-  return product
+  const product = urlParams.get("product");
+  return product;
 }
 
-export function renderWithTemplate(template, parentElement, data, position = 'afterbegin', clear = false){
-  parentElement.insertAdjacentHTML("afterbegin",template);
-  if (callback){
-    callback(data)
+export function renderWithTemplate(
+  template,
+  parentElement,
+  data,
+  position = "afterbegin",
+  clear = false
+) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+  if (callback) {
+    callback(data);
   }
-  
 }
 
-async function loadTemplate(path){
+async function loadTemplate(path) {
   let fetchedData = await fetch(path);
   let text = await fetchedData.text();
-  return text
+  return text;
 }
 
-export async function loadHeaderFooter(){
- const templateForHeader=await loadTemplate("../partials/header.html");
- const elementForHeader=document.querySelector("#main-header")
- const templateForFooter=await loadTemplate("../partials/footer.html");
- const elementForFooter=document.querySelector("#main-footer");
+export async function loadHeaderFooter() {
+  const templateForHeader = await loadTemplate("../partials/header.html");
+  const elementForHeader = document.querySelector("#main-header");
+  const templateForFooter = await loadTemplate("../partials/footer.html");
+  const elementForFooter = document.querySelector("#main-footer");
 
-  renderWithTemplate(templateForHeader,elementForHeader)
-  renderWithTemplate(templateForFooter,elementForFooter)
+  renderWithTemplate(templateForHeader, elementForHeader);
+  renderWithTemplate(templateForFooter, elementForFooter);
+}
+
+export function alertMessage(message, scroll = true, duraction = 3000) {
+  const alert = document.createElement("div");
+  alert.classList.add("alert");
+
+  alert.innerHTML = `<p>${message}</p><span>X</span>`;
+
+  alert.addEventListener("click", function (e) {
+    if (e.target.tagName == "SPAN") {
+      MediaDeviceInfo.removeChild(this);
+    }
+  });
+
+  const main = document.querySelector("main");
+  main.prepend(alert);
+  if (scroll) window.scrollTo(0, 0);
 }
